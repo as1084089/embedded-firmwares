1178bed287f23338adfcd7ea7630ed49e5a35852
as1084089 <as1084089@gmail.com>
2025-05-31 01:12:24 +0900

    [LM3S6965] add character & string print functions through uart

--- arm-cortex-m3/LM3S6965/src/main.c
+++ arm-cortex-m3/LM3S6965/src/main.c
@@ -1,11 +1,22 @@
 // main.c
 volatile unsigned int *UART0_DR = (unsigned int *)0x4000C000;
 
+void uart_putc(char c);
+void uart_puts(const char *str);
+
 int main() {
     const char *hello = "Hello QEMU!\n";
-    while (*hello) {
-        *UART0_DR = *hello++;
-    }
+    uart_puts(hello);
     while (1);
     return 0;
+}
+
+void uart_putc(char c) {
+    *UART0_DR = c;
+}
+
+void uart_puts(const char *str) {
+    while (*str) {
+        uart_putc(*str++);
+    }
 }
